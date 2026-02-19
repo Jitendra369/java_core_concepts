9e361e96fcabb2782fce11352a8bdf67fc273d8d
jitendra <onekb.twotb@gmail.com>
2026-02-19 10:57:01 +0530

    updated code

--- src/thread/MainClass2.java
+++ src/thread/MainClass2.java
@@ -1,28 +1,29 @@
 package thread;
 
+import java.util.List;
 import java.util.concurrent.*;
 
 public class MainClass2 {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
-//        ExecutorService executorService = Executors.newFixedThreadPool(2);
-
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
         CompletionService<Integer> completionService = new ExecutorCompletionService<>(executorService);
 
-        Future<Integer> future = completionService.submit(new MyCallableTask());
-        Future<Integer> feature2 = completionService.submit(new Callable<Integer>() {
-            @Override
-            public Integer call() throws Exception {
-                return 115;
-            }
-        });
+        List<Callable<Integer>> tasks = List.of(
+                new MyCallableTask(),
+                () -> 116,
+                () -> 113,
+                () -> 12
+        );
 
+        for (Callable<Integer> callable : tasks) {
+            completionService.submit(callable);
+        }
 
-        Integer result  = future.get();
-        Integer i = feature2.get();
-        System.out.println("result is : "+ result);
-        System.out.println("result2 is : "+ i);
-        System.out.println("waiting for the result  ");
+        // getting the result
+        for (int i = 0; i < tasks.size(); i++) {
+            Future<Integer> futureResult = completionService.take();
+            System.out.println(futureResult.get());
+        }
         executorService.shutdown();
     }
 }
